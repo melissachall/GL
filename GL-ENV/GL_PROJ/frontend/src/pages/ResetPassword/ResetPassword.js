6832f4b323df808c5b2a9e88133deb3e622975a5
KamelTouati <kameltouati19.work@gmail.com>
2023-01-29 15:25:48 +0100

    add reset-password page

--- GL-ENV/GL_PROJ/frontend/src/pages/ResetPassword/ResetPassword.js
+++ GL-ENV/GL_PROJ/frontend/src/pages/ResetPassword/ResetPassword.js
@@ -25,23 +25,19 @@ const ResetPassword = ({ reset_password }) => {
     }
 
     return (
-        <div className='container mt-5'>
-            <h1>Request Password Reset:</h1>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='email'
-                        placeholder='Email'
-                        name='email'
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required
-                    />
+        <>
+            <div className='flex justify-center'>
+                <div className="mt-[50px] p-10">
+                    <h1>Request Password Reset:</h1>
+                    <form onSubmit={e => onSubmit(e)}>
+                        <div className="mb-6">
+                            <input type="email" id="email" name='email' value={email} onChange={e => onChange(e)} className="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  lg:w-[500px]" placeholder="Email" required />
+                        </div>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reset Password</button>
+                    </form>
                 </div>
-                <button className='btn btn-primary' type='submit'>Reset Password</button>
-            </form>
-        </div>
+            </div>
+        </>
     );
 };
 

6941a14174a9d8dfc0fb897d559575f533f469df
KamelTouati <kameltouati19.work@gmail.com>
2023-01-29 01:02:38 +0100

    some changes

--- GL-ENV/GL_PROJ/GL_PROJ/settings.py
+++ GL-ENV/GL_PROJ/GL_PROJ/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kameltechdz.19@gmail.com'
-EMAIL_HOST_PASSWORD = 'svjepddyrrluroyw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
